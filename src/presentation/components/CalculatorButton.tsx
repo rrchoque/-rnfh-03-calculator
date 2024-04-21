80af12dc492cd17d96d32051e099e96adbf9558b
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-04-21 06:01:20 -0400

    Se agregó el parametro onPress a los botones

--- src/presentation/components/CalculatorButton.tsx
+++ src/presentation/components/CalculatorButton.tsx
@@ -7,18 +7,20 @@ interface Props {
   color?: string;
   doubleSize?: boolean;
   blackText?: boolean;
+  onPress: () => void;
 }
-
-
 
 export const CalculatorButton = ({
   label,
   color = colors.darkGray,
   doubleSize = false,
   blackText = false,
+  onPress
+
 }:Props) => {
   return (
     <Pressable 
+      onPress={ () => onPress() }
       style={ ({pressed}) => ({
       ...styles.button,
       backgroundColor: color,
